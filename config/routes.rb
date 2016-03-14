b78c87a7130dd873e165e9704e7be9ecf6b0de69
sophiakc <sophiakc@gmail.com>
2016-03-14 17:16:22 +0100

    création page trombi worriers + content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/" => 'home#index'
+  get "/worriers" => 'worriers#worriers'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
